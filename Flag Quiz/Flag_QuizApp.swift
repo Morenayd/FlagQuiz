cdded884df537437935b495318a0f7c059303021
Jesutofunmi <tofdebby@gmail.com>
2024-02-12 17:25:27 +0100

    Initial Commit

---
+++ Flag Quiz/Flag_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_QuizApp.swift
+//  Flag Quiz
+//
+//  Created by Jesutofunmi Adewole on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flag_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
